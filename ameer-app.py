03f5d940ab7b3f3abede7eda3a9c0d9ddc1fd9d4
mr7innovator7in <76661769+mr7innovator7in@users.noreply.github.com>
2020-12-30 22:41:28 +0530

    Update ameer-app.py

--- ameer-app.py
+++ ameer-app.py
@@ -1,19 +1,17 @@
-# Short code
 import tweepy
-consumer_key = '6ywz7k7gwFrT5btrcAodOlwBa'
-consumer_secret = 'ch34ohAxW6tHgZWPAKlDr3FOH5e7NhjL4KByJ5hm5e1GRYCCJT'
+api_key = '6ywz7k7gwFrT5btrcAodOlwBa'
+api_secret_key = 'ch34ohAxW6tHgZWPAKlDr3FOH5e7NhjL4KByJ5hm5e1GRYCCJT'
 access_token = '1339936871091081221-3IXURJ1G0HpLzoMijGqVQMeQoHzJW5'
 secret_access_token = '9g8NIOn5fPKPgTEPQ4TyRRBZm0z0lrH1q8XYueyrrGg0q'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, secret_access_token)
-api = tweepy.API(auth)
-print('Everything is fine')
 
+api = tweepy.API(auth)
 import time
-while True:
-  user = api.get_user('blabla21932776')
-  f = user.followers_count
-  api.update_profile(name=f'bla bla {f} Followers')
-  print(f'bla bla{f} Followers')
-  time.sleep(60)
+while True: # to make this forever
+  user= api.get_user('blabla21932776')
+  follower = user.followers_count
+  api.update_profile(name = f'bla bla {follower} followers')
+  print(f'bla bla {follower} followers')
+  time.sleep(60) #this process takes 60s to update (limitation) 
