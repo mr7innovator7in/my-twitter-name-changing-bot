333979c0f28aa4b9c632fedebfb81fd7f02fc528
mr7innovator7in <76661769+mr7innovator7in@users.noreply.github.com>
2020-12-31 15:47:39 +0530

    Update ameer-app.py

--- ameer-app.py
+++ ameer-app.py
@@ -1,15 +1,15 @@
 import tweepy
 api_key = '6ywz7k7gwFrT5btrcAodOlwBa'
 api_secret_key = 'ch34ohAxW6tHgZWPAKlDr3FOH5e7NhjL4KByJ5hm5e1GRYCCJT'
-access_token = '1339936871091081221-3IXURJ1G0HpLzoMijGqVQMeQoHzJW5'
-secret_access_token = '9g8NIOn5fPKPgTEPQ4TyRRBZm0z0lrH1q8XYueyrrGg0q'
+access_token = '1339936871091081221-2cJRKgr2zt4OIE02vMAclytbcECXzG'
+secret_access_token = 'uPCMMdDePNkHak5FE3THvJLXlw5W8UXDLwNGCj1DLK0Nf'
 
 auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, secret_access_token)
 
 api = tweepy.API(auth)
 import time
-while True: # to make this forever
+while True:
   user= api.get_user('blabla21932776')
   follower = user.followers_count
   api.update_profile(name = f'bla bla {follower} followers')
